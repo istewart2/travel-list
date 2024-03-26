fd2f28db34a77d927d061164ca5da86792528fdf
Iain Stewart <iainstewart08@gmail.com>
2024-03-26 16:22:07 +0000

    Setup form to add itemd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { useState } from 'react';
+
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
   return (
     <div className='app'>
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -13,16 +21,70 @@ function Logo() {
   return <h1>🏝️ Far away 🧳</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    // stop page from reloading
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription('');
+    setQuantity(1);
+  }
+
   return (
-    <div className='add-form'>
+    <form className='add-form' onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type='text'
+        placeholder='Item...'
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList({ items }) {
+  return (
+    <div className='list'>
+      <ul>
+        {items.map((item) => (
+          <Item item={item} key={item.id} />
+        ))}
+      </ul>
     </div>
   );
 }
 
-function PackingList() {
-  return <div className='list'>LIST</div>;
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.description} {item.quantity}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
